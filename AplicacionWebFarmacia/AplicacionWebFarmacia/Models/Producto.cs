9f07c980c05c3d23b54a0dac03c04aaf0d566f6b
Goldie3 <brunoaltamirano978@gmail.com>
2026-05-07 23:23:40 -0400

    Agregar archivos de proyecto.

---
+++ AplicacionWebFarmacia/AplicacionWebFarmacia/Models/Producto.cs
@@ -0,0 +1,23 @@
+﻿namespace AplicacionWebFarmacia.Models
+{
+    public class Producto
+    {
+        public int idProducto { get; set; }
+        public string nombreProducto { get; set; }
+        public DateTime fechaIngreso { get; set; }
+        public int cantidadProducto { get; set; }
+        public int precio { get; set; }
+
+        public string descripcion { get; set; }
+    public Producto(int idProducto, string nombreProducto, DateTime fechaIngreso, int cantidadProducto, int precio, string descripcion)
+        {
+            this.idProducto = idProducto;
+            this.nombreProducto = nombreProducto;
+            this.fechaIngreso = fechaIngreso;
+            this.cantidadProducto = cantidadProducto;
+            this.precio = precio;
+            this.descripcion = descripcion;
+        }
+        
+    }
+}
